deeaeaff7e4963acad5ef1bb929ff3c8bfd25e4d
Fabian Sebban <fa.sebban@gmail.com>
2017-03-17 11:02:59 +0100

    models project et team

--- models/Project.js
+++ models/Project.js
@@ -1,11 +1,31 @@
+const timestamps = require('mongoose-timestamps');
+
 module.exports = (server) => {
     const Schema = server.mongoose.Schema;
     const ProjectSchema = new Schema({
         name: {
             type: String,
             required: true
-        }
+        },
+
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+
+        team: {
+          type: Schema.Types.ObjectId,
+          ref: 'Team'
+        },
+
+        todos: [{
+          type: Schema.Types.ObjectId,
+          ref: 'Todo'
+        }]
     });
+
+    ProjectSchema.plugin(timestamps);
 
     return server.mongoose.model('Project', ProjectSchema);
 }
--- models/Team.js
+++ models/Team.js
@@ -1,11 +1,32 @@
+const timestamps = require('mongoose-timestamps');
+
 module.exports = (server) => {
+
     const Schema = server.mongoose.Schema;
+
     const TeamSchema = new Schema({
         name: {
             type: String,
             required: true
-        }
+        },
+        creator: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        project: {
+          type: Schema.Types.ObjectId,
+          ref: 'Project',
+          required: true
+        },
+        members: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
     });
 
+    TeamSchema.plugin(timestamps);
     return server.mongoose.model('Team', TeamSchema);
 }
