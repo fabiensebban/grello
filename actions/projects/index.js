13c7f55a9cb4760422c4fd8f04075a550cf86145
ghansum <ghansumn@gmail.com>
2017-03-17 11:48:36 +0100

    add actions projects

--- actions/projects/index.js
+++ actions/projects/index.js
@@ -1,5 +1,6 @@
 module.exports = (server) => {
     const Project = server.models.Project;
+    const User = server.models.User;
 
     return {
         create,
@@ -10,11 +11,42 @@ module.exports = (server) => {
     }
 
     function create(req, res, next) => {
+      let user = null;
+      return User.findById(req.user.id)
+          .then(server.utils.ensureOne)
+          .catch(server.utils.reject(403, 'invalid.user'))
+          .then(createProject)
+          .then(setCreator)
+          .then(persist)
+          .then(res.commit)
+          .catch(res.error);
+
+
+        function createProject(data) {
+            user = data;
+            return new Project(req.body);
+        }
+
+        function setCreator(project) {
+            project.owner = req.user.id;
+            return project;
+        }
+
+        function persist(project) {
+            return project.save()
+                .then(returnProject);
+
+            function returnProject() {
+                return project;
+            }
+        }
 
     }
 
     function list(req, res, next) => {
-
+      Project.find()
+            .then(res.commit)
+            .catch(res.error);
     }
 
     function show(req, res, next) => {
