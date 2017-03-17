29f085d79603f59317e61eb187c52f5b3e2b3ff8
couini <amliebarre@gmail.com>
2017-03-17 11:15:45 +0100

    create CRUD skeleton for Project and Team

---
+++ actions/projects/index.js
@@ -0,0 +1,32 @@
+module.exports = (server) => {
+    const Project = server.models.Project;
+
+    return {
+        create,
+        list,
+        show,
+        update,
+        remove
+    }
+
+    function create(req, res, next) => {
+
+    }
+
+    function list(req, res, next) => {
+
+    }
+
+    function show(req, res, next) => {
+
+    }
+
+    function update(req, res, next) => {
+
+    }
+
+    function remove(req, res, next) => {
+
+    }
+
+}
---
+++ actions/teams/index.js
@@ -0,0 +1,31 @@
+module.exports = (server) => {
+    const Team = server.models.Task;
+
+    return {
+        create,
+        list,
+        show,
+        update,
+        remove
+    }
+
+    function create(req, res, next) => {
+
+    }
+
+    function list(req, res, next) => {
+
+    }
+
+    function show(req, res, next) => {
+
+    }
+
+    function update(req, res, next) => {
+
+    }
+
+    function remove(req, res, next) => {
+
+    }
+}
