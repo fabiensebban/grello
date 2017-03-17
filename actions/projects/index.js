07e4d0ffafed33cb04ff2b485097b68975854030
couini <amliebarre@gmail.com>
2017-03-17 11:49:27 +0100

    add routes projects and teams

--- actions/projects/index.js
+++ actions/projects/index.js
@@ -1,32 +1,3 @@
 module.exports = (server) => {
-    const Project = server.models.Project;
-
-    return {
-        create,
-        list,
-        show,
-        update,
-        remove
-    }
-
-    function create(req, res, next) => {
-
-    }
-
-    function list(req, res, next) => {
-
-    }
-
-    function show(req, res, next) => {
-
-    }
-
-    function update(req, res, next) => {
-
-    }
-
-    function remove(req, res, next) => {
-
-    }
 
 }
